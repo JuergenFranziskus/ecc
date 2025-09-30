9dbbbce5d013c7ccccf9b5a37f19292495f7ed9f
Juergen Franziskus <felix.hppt@gmail.com>
2025-09-30 19:39:44 +0200

    Invoke external gnu preprocessor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    const SRC_FILE: &str = "main.c";
+    let src = invoke_preprocessor(SRC_FILE).unwrap();
+    println!("{src}");
+}
+
+fn invoke_preprocessor(file: &str) -> Result<String, ()> {
+    let out = Command::new("cpp")
+        .arg("-nostdinc")
+        .arg("-undef")
+        .arg(file)
+        .arg("-")
+        .output()
+        .unwrap();
+    if !out.status.success() {
+        let stderr = String::from_utf8_lossy(&out.stderr);
+        eprintln!("Preprocessor failed:");
+        eprintln!("{stderr}");
+        return Err(());
+    }
+
+    match String::from_utf8(out.stdout) {
+        Ok(out) => Ok(out),
+        Err(err) => {
+            eprintln!("Preprocessor output is not UTF-8: {err}");
+            Err(())
+        }
+    }
 }
